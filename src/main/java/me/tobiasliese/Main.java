c96e1dd300bf6219f25aaceacd16de89d4c0c5bd
tliese <tobias5@uni-bremen.de>
2024-08-15 15:33:21 +0200

    First working PoC :)

--- src/main/java/me/tobiasliese/Main.java
+++ src/main/java/me/tobiasliese/Main.java
@@ -1,7 +1,19 @@
 package me.tobiasliese;
 
+import org.parsers.java.JavaParser;
+import org.parsers.java.ast.PrimaryExpression;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public static void main(String[] args) throws IOException {
+		Path path = Path.of("/home/tliese/JX/src/main/resources/example.jx");
+
+		var parser = new JavaParser(String.join("\n", Files.readAllLines(path)));
+		var root = parser.Root();
+		root.childrenOfType(PrimaryExpression.class).forEach(System.out::println);
+		parser.rootNode().children(false).stream().forEach(System.out::println);
 	}
 }
